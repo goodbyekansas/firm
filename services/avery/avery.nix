a3b4ce9c1841ca8908e6bf7ce4236111f2652952
Sakarias Johansson <sakarias.johansson@goodbyekansas.com>
2020-03-03 16:37:38 +0100

    👷‍♀️ Add CI
    This also completes our nix derivation for avery to build and
    output a package.

--- services/avery/avery.nix
+++ services/avery/avery.nix
@@ -1,15 +1,40 @@
 let
-    moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-    nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-in
-    with nixpkgs;
-    stdenv.mkDerivation {
-        name = "avery";
-        buildInputs = [
-            nixpkgs.latest.rustChannels.stable.rust
-        ];
+  moz_overlay = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "avery";
+  src = builtins.filterSource
+    (path: type: (type != "directory" || baseNameOf path != "target")) ./.;
 
-        shellHook = ''
-        export PROTOBUF_DEFINITIONS_LOCATION=../../protocols
-        '';
-    }
+  buildInputs = with nixpkgs; [ cacert latest.rustChannels.stable.rust ];
+
+  doCheck = true; # TODO: When nedryland takes over the world remove this.
+
+  buildPhase = ''
+    export HOME=$PWD
+    cargo build --release
+  '';
+
+  checkPhase = ''
+    export HOME=$PWD
+    cargo test
+
+    cargo fmt -- --check
+
+    cargo clippy
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp target/release/avery $out/bin
+  '';
+
+  # always want backtraces when building or in dev
+  RUST_BACKTRACE = 1;
+  PROTOBUF_DEFINITIONS_LOCATION=../../protocols;
+  shellHook = ''
+    export PROTOBUF_DEFINITIONS_LOCATION=../../protocols
+  '';
+}
