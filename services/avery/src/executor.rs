04b25453805734375da67c84fc4ef2dd66449e0e
Albert Cervin <albert.cervin@goodbyekansas.com>
2020-03-09 10:20:23 +0100

    ✨ Use proper errors inside executor

--- services/avery/src/executor.rs
+++ services/avery/src/executor.rs
@@ -1,4 +1,5 @@
 use serde_json::{self, Value};
+use thiserror::Error;
 
 use crate::proto::execute_response::Result as ProtoResult;
 use crate::proto::{ArgumentType, FunctionInput};
@@ -25,35 +26,20 @@ impl FunctionExecutor for WasmExecutor {
     }
 }
 
-pub fn lookup_executor(name: &str) -> Result<Box<dyn FunctionExecutor>, String> {
+pub fn lookup_executor(name: &str) -> Result<Box<dyn FunctionExecutor>, ExecutorError> {
     match name {
         "maya" => Ok(Box::new(MayaExecutor {})),
         "wasm" => Ok(Box::new(WasmExecutor {})),
-        ee => Err(format!("Failed to find execution environment: {}", ee)),
+        ee => Err(ExecutorError::ExecutorNotFound(ee.to_owned()).into()),
     }
 }
 
-fn invalid_arg_type(arg_type: i32) -> String {
-    format!(
-        "Invalid argument type \"{}\". (protobuf enum definition is most likely outdated)",
-        arg_type
-    )
-}
-
-fn valid_to_result(valid: bool, error_msg: String) -> Result<(), String> {
-    if valid {
-        Ok(())
-    } else {
-        Err(error_msg)
-    }
-}
-
-pub fn validate_args<'a, I>(inputs: I, args: &str) -> Result<(), String>
+pub fn validate_args<'a, I>(inputs: I, args: &str) -> Result<(), ExecutorError>
 where
     I: IntoIterator<Item = &'a FunctionInput>,
 {
-    let parsed_args: Value =
-        serde_json::from_str(args).map_err(|e| format!("Failed to parse json args: {}", e))?;
+    let parsed_args: Value = serde_json::from_str(args)
+        .map_err(|e| ExecutorError::InvalidArgumentFormat(e.to_string()))?;
     // TODO: This will abort as soon as it finds a bad arg
     // Would be nice to make it show all errors instead of first.
 
@@ -63,28 +49,55 @@ where
         .map(|input| {
             parsed_args
                 .get(&input.name)
-                .ok_or(format!("Failed to find input \"{}\".", &input.name))
+                .ok_or(ExecutorError::RequiredArgumentMissing(input.name.clone()))
                 .and_then(|parsed_arg| {
                     let tp = input.r#type;
                     ArgumentType::from_i32(tp)
                         .and_then(|at| {
                             Some(match at {
-                                ArgumentType::String => parsed_arg.is_string(),
-                                ArgumentType::Bool => parsed_arg.is_boolean(),
-                                ArgumentType::Int => parsed_arg.is_i64(),
-                                ArgumentType::Float => parsed_arg.is_f64(),
+                                ArgumentType::String => (parsed_arg.is_string(), "string"),
+                                ArgumentType::Bool => (parsed_arg.is_boolean(), "bool"),
+                                ArgumentType::Int => (parsed_arg.is_i64(), "int"),
+                                ArgumentType::Float => (parsed_arg.is_f64(), "float"),
                             })
                         })
-                        .map_or(Err(invalid_arg_type(tp)), |valid| {
-                            valid_to_result(
-                                valid,
-                                format!(
-                                    r#"Argument "{}" expected type "{}". Failed to parse "{}" to type."#,
-                                    input.name, tp, parsed_arg
-                                ),
-                            )
-                        })
+                        .map_or(
+                            Err(ExecutorError::OutOfRangeArgumentType(tp)),
+                            |(valid, type_name)| {
+                                if valid {
+                                    Ok(())
+                                } else {
+                                    Err(ExecutorError::MismatchedArgumentType {
+                                        argument_name: input.name.clone(),
+                                        expected: type_name.to_owned(),
+                                        value: parsed_arg.to_string(),
+                                    })
+                                }
+                            },
+                        )
                 })
         })
         .collect()
+}
+
+#[derive(Error, Debug)]
+pub enum ExecutorError {
+    #[error("Failed to find executor for execution environment \"{0}\"")]
+    ExecutorNotFound(String),
+
+    #[error("Failed to interpret passed arguments: {0}")]
+    InvalidArgumentFormat(String),
+
+    #[error("Out of range argument type found: {0}. Protobuf definitions out of date?")]
+    OutOfRangeArgumentType(i32),
+
+    #[error("Argument \"{argument_name}\" has unexpected type. Failed to parse \"{value}\" to {expected}")]
+    MismatchedArgumentType {
+        argument_name: String,
+        expected: String,
+        value: String,
+    },
+
+    #[error("Failed to find required argument {0}")]
+    RequiredArgumentMissing(String),
 }
