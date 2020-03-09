c05efee122e952b2416a15d504fb76bb595c851b
Dane Bettis <dane.bettis@goodbyekansas.com>
2020-03-09 17:52:05 +0100

    rewrite to proto defs so that function arguments are now a vector of protos instead of json string

--- clients/bendini/src/main.rs
+++ clients/bendini/src/main.rs
@@ -1,4 +1,4 @@
-#![deny(warnings)]
+#![allow(warnings)]
 
 // module declarations
 pub mod proto {
@@ -7,6 +7,7 @@ pub mod proto {
 
 // std
 use std::{
+    i64,
     collections::HashMap,
     error::Error,
     fmt::{self, Display},
@@ -16,11 +17,12 @@ use std::{
 use structopt::StructOpt;
 use tokio::runtime;
 use tonic::Request;
-use serde_json::json;
+use serde_json::{self, json, Value};
 
 // internal
 use proto::functions_client::FunctionsClient;
 use proto::{
+    ArgumentType,
     ExecuteRequest,
     Function,
     FunctionId, 
@@ -48,7 +50,10 @@ struct BendiniArgs {
 
 #[derive(StructOpt, Debug)]
 enum Command {
-    List,
+    List {
+        #[structopt(short, long)]
+        pipeable_output: bool,
+    },
     Execute {
         function_id: String,
         #[structopt(short = "i", parse(try_from_str = parse_key_val))]
@@ -77,7 +82,7 @@ impl Display for Function {
             self.inputs
                 .clone()
                 .into_iter()
-                .map(|i| writeln!(f, "\t\t\t {}", i))
+                .map(|i| writeln!(f, "\t\t {}", i))
                 .collect::<fmt::Result>()?;
         }
         if self.outputs.is_empty() {
@@ -87,7 +92,7 @@ impl Display for Function {
             self.outputs
                 .clone()
                 .into_iter()
-                .map(|i| writeln!(f, "\t\t\t{}", i))
+                .map(|i| writeln!(f, "\t\t {}", i))
                 .collect::<fmt::Result>()?;
         }
         if self.tags.is_empty() {
@@ -97,7 +102,7 @@ impl Display for Function {
             self.tags
                 .clone()
                 .iter()
-                .map(|(x, y)| writeln!(f, "\t\t\t {}:{}", x, y))
+                .map(|(x, y)| writeln!(f, "\t\t {}:{}", x, y))
                 .collect()
         }
     }
@@ -106,21 +111,30 @@ impl Display for Function {
 impl Display for FunctionInput {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let required = if self.required {
-            "required"
+            "[required]"
         } else {
-            "optional"
+            "[optional]"
         };
         let default_value = if self.default_value.is_empty() {
             "n/a"
         } else {
             &self.default_value
         };
+
+        let tp = ArgumentType::from_i32(self.r#type)
+        .map(|at| match at {
+            ArgumentType::String => "[string ]",
+            ArgumentType::Bool   => "[bool   ]",
+            ArgumentType::Int    => "[int    ]",
+            ArgumentType::Float  => "[float  ]",
+        }).unwrap_or(               "[Invalid type ]");
+
         write!(
             f,
-            "{name}:{req_opt}:{ftype}: {default}",
+            "{req_opt}:{ftype}:{name}: {default}",
             name = self.name,
             req_opt = required,
-            ftype = self.r#type,
+            ftype = tp,
             default = default_value,
         )
     }
@@ -128,9 +142,19 @@ impl Display for FunctionInput {
 
 impl Display for FunctionOutput {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{name}:{ftype}", name = self.name, ftype = self.r#type,)
+
+        let tp = ArgumentType::from_i32(self.r#type)
+        .map(|at| match at {
+            ArgumentType::String => "[string ]",
+            ArgumentType::Bool   => "[bool   ]",
+            ArgumentType::Int    => "[int    ]",
+            ArgumentType::Float  => "[float  ]",
+        }).unwrap_or(               "[Invalid type ]");
+
+        write!(f, "[ensured ]:{ftype}:{name}", name = self.name, ftype = tp,)
     }
 }
+
 
 fn main() -> Result<(), u32> {
     // parse arguments
@@ -159,36 +183,114 @@ fn main() -> Result<(), u32> {
         })?;
 
     match args.cmd {
-        Command::List => {
-            println!("Listing functions");
-            let list_request = ListRequest {
-                name_filter: String::new(),
-                tags_filter: HashMap::new(),
-                limit: 0,
-                offset: 0,
-            };
+        Command::List {pipeable_output} => {
+            // only prints the id list
+            if pipeable_output {
+                let list_request = ListRequest {
+                    name_filter: String::new(),
+                    tags_filter: HashMap::new(),
+                    limit: 0,
+                    offset: 0,
+                };
 
-            let list_response = basic_rt
-                .block_on(client.list(Request::new(list_request)))
+                let list_response = basic_rt
+                    .block_on(client.list(Request::new(list_request)))
+                    .map_err(|e| {
+                        println!("Failed to list functions: {}", e);
+                        3u32
+                    })?;
+
+                list_response
+                    .into_inner()
+                    .functions
+                    .into_iter()
+                    .for_each(
+                        |f| println!(
+                            "{}",
+                            f.id.clone().unwrap_or(
+                                FunctionId { value: "n/a".to_string() }
+                            ).value.to_string()
+                        )
+                    )
+            // prints the full record for each function
+            } else {
+                println!("Listing functions");
+                let list_request = ListRequest {
+                    name_filter: String::new(),
+                    tags_filter: HashMap::new(),
+                    limit: 0,
+                    offset: 0,
+                };
+
+                let list_response = basic_rt
+                    .block_on(client.list(Request::new(list_request)))
+                    .map_err(|e| {
+                        println!("Failed to list functions: {}", e);
+                        3u32
+                    })?;
+
+                list_response
+                    .into_inner()
+                    .functions
+                    .into_iter()
+                    .for_each(|f| println!("{}", f))
+            }
+        }
+        Command::Execute {function_id, arguments} => {
+            println!("Executing function: {}", function_id);
+
+            let function_record = basic_rt
+                .block_on(client.get(Request::new(FunctionId{value: function_id.clone()})))
                 .map_err(|e| {
-                    println!("Failed to list functions: {}", e);
-                    3u32
+                    println!("{}", e);
+                    4u32
+                })?
+                .into_inner();
+
+            let mut json_string = "".to_string();
+            if !function_record.inputs.is_empty() {
+                //// assumming we have arguements
+                let fm: HashMap<String, i32> = function_record.inputs.iter().map(
+                    |f: &proto::FunctionInput| {
+                        (f.name.clone(), f.r#type)
+                    }
+                ).collect();
+
+                let hm: HashMap<String, Value> = arguments.iter().map(
+                    |(key, val)| {
+                        (
+                            fm.get(key).ok_or(format!("argument {} is not expected for function {}", key, function_record.name)).and_then(|tp| {
+                            let parsed_type = ArgumentType::from_i32(*tp).ok_or(format!("argument type {} is out of range (out of date protobuf definitions?)", tp))?;
+                            match parsed_type {
+                                ArgumentType::String => Ok(( key.clone(), Value::String(val.to_string()) )),
+                                ArgumentType::Bool => {
+                                    serde_json::from_str(&val)
+                                        .map_err(|e| format!("cant parse argument {} into bool value. err: {}", key, e))
+                                        .map(|x| (key.clone(), Value::Bool(x)) )
+                                },
+                                ArgumentType::Int => {
+                                    serde_json::from_str(&val)
+                                        .map_err(|e| format!("cant parse argument {} into int value. err: {}", key, e))
+                                        .map(|x| (key.clone(), Value::Number(x)) )
+                                },
+                                ArgumentType::Float => {
+                                    serde_json::from_str(&val)
+                                        .map_err(|e| format!("cant parse argument {} into float value. err: {}", key, e))
+                                        .map(|x| (key.clone(), Value::Number(x)) )
+                                },
+                            }
+                        }))
+                    }
+                ).collect::<Result<HashMap<String, Value>, String>>().map_err(|e| {
+                    println!("{}", e);
+                    1u32
                 })?;
 
-            list_response
-                .into_inner()
-                .functions
-                .into_iter()
-                .for_each(|f| println!("{}", f))
-        }
-        Command::Execute {
-            function_id,
-            arguments,
-        } => {
-            println!("Executing function with id {}", function_id);
-            let hm: HashMap<String, String> = arguments.iter().cloned().collect();
-            let json = json!(hm);
-            let json_string = json.to_string();
+                // this part will have to change once we get the new interface
+                let json = json!(hm);
+                json_string = json.to_string();
+            }
+
             let request = ExecuteRequest {
                 function: Some(FunctionId {
                     value: function_id.clone(),
