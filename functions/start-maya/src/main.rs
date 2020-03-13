f1e161ac584abc5f10ef8d8bb12343fa8aed7f50
Albert Cervin <albert.cervin@goodbyekansas.com>
2020-03-13 13:11:01 +0100

    ✨ Now we can start maya!

--- functions/start-maya/src/main.rs
+++ functions/start-maya/src/main.rs
@@ -1,3 +1,22 @@
+#[link(wasm_import_module = "gbk")]
+extern "C" {
+    fn start_process(string_ptr: *const u8, len: usize) -> i64;
+}
+
+pub mod gbk {
+    use crate::start_process;
+    pub fn start_dcc(name: &str) -> bool {
+        let ri64 = unsafe { start_process(name.as_ptr(), name.len()) };
+        return ri64 != 0;
+    }
+}
+
 fn main() {
-    println!("Hello! Someone is rust wasmer!");
+    println!("Hello! I will start maya from WASI now!");
+
+    if gbk::start_dcc("maya") {
+        println!("started maya")
+    } else {
+        println!("failed to start maya because of reasons")
+    };
 }
