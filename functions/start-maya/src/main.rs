5d79a48679422747ef8a53f4e2d863ea5fbcec31
Albert Cervin <albert.cervin@goodbyekansas.com>
2020-03-13 13:35:41 +0100

    ⚡ Make start dcc a bit more generic

--- functions/start-maya/src/main.rs
+++ functions/start-maya/src/main.rs
@@ -1,12 +1,12 @@
 #[link(wasm_import_module = "gbk")]
 extern "C" {
-    fn start_process(string_ptr: *const u8, len: usize) -> i64;
+    fn start_host_process(string_ptr: *const u8, len: usize) -> i64;
 }
 
 pub mod gbk {
-    use crate::start_process;
-    pub fn start_dcc(name: &str) -> bool {
-        let ri64 = unsafe { start_process(name.as_ptr(), name.len()) };
+    use crate::start_host_process as raw_start_host_process;
+    pub fn start_host_process(name: &str) -> bool {
+        let ri64 = unsafe { raw_start_host_process(name.as_ptr(), name.len()) };
         return ri64 != 0;
     }
 }
@@ -14,7 +14,7 @@ pub mod gbk {
 fn main() {
     println!("Hello! I will start maya from WASI now!");
 
-    if gbk::start_dcc("maya") {
+    if gbk::start_host_process("/usr/autodesk/maya2019/bin/maya") {
         println!("started maya")
     } else {
         println!("failed to start maya because of reasons")
