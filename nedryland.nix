618fe00b531f1f8e93cfc3369b8996238d90b94e
Albert Cervin <albert@acervin.com>
2020-03-23 09:09:27 +0100

    ✨ Use the new nedryland init
    This makes the project setup less verbose and custom.

--- nedryland.nix
+++
@@ -1,8 +0,0 @@
-if builtins.getEnv "NEDRYLAND_PATH" != "" then
-  (./. + "/${builtins.getEnv "NEDRYLAND_PATH"}")
-else
-  builtins.fetchGit {
-    name = "nedryland";
-    url = "git@github.com:goodbyekansas/nedryland.git";
-    ref = "4505394a1d52de47483f8205c016aa99c5c6fa19";
-  }
