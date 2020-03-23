2d5024091ea23a8178ca29f8f06a45418ee736d6
Simon Rainerson <simonr88@gmail.com>
2020-03-23 15:03:06 +0100

    🔨 Use client convenience functions

--- clients/bendini/bendini.nix
+++ clients/bendini/bendini.nix
@@ -1,47 +1,5 @@
 { pkgs, base }:
-with pkgs;
-base.mkComponent {
-  package = stdenv.mkDerivation {
-    name = "bendini";
-    src = builtins.filterSource
-      (path: type: (type != "directory" || baseNameOf path != "target")) ./.;
-
-    buildInputs = with pkgs; [
-      cacert
-      (latest.rustChannels.stable.rust.override { extensions = [ "rust-src" ]; })
-    ];
-
-    # this is needed on NixOS but does not hurt on other
-    # OSes either
-    PROTOC = "${pkgs.protobuf}/bin/protoc";
-
-    buildPhase = ''
-      export HOME=$PWD
-      cargo build --release
-    '';
-
-    checkPhase = ''
-      if [ -z $IN_NIX_SHELL ]; then
-        export HOME="$PWD"
-      fi
-
-      cargo test
-
-      cargo fmt -- --check
-
-      cargo clippy
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp target/release/bendini $out/bin
-    '';
-
-    # always want backtraces when building or in dev
-    RUST_BACKTRACE = 1;
-    PROTOBUF_DEFINITIONS_LOCATION = ../../protocols;
-    shellHook = ''
-      export PROTOBUF_DEFINITIONS_LOCATION=../../protocols
-    '';
-  };
+base.languages.rust.mkRustClient {
+  name = "bendini";
+  src = ./.;
 }
