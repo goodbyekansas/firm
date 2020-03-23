bb4a1dce4bf577db1e92a53aabc737ed38819439
Simon Rainerson <simonr88@gmail.com>
2020-03-23 17:05:22 +0100

    🔨 Use conveniece function for rust service

--- services/avery/avery.nix
+++ services/avery/avery.nix
@@ -1,48 +1,9 @@
 { pkgs, base, inputFunctions }:
 with pkgs;
-base.mkComponent {
-  package = stdenv.mkDerivation {
-    name = "avery";
-    src = builtins.filterSource
-      (path: type: (type != "directory" || baseNameOf path != "target")) ./.;
+base.languages.rust.mkRustService {
+  name = "avery";
+  src = ./.;
 
-    buildInputs = with pkgs; [
-      cacert
-      (latest.rustChannels.stable.rust.override { extensions = [ "rust-src" ]; })
-    ];
-
-    # TODO: This gives a nice workflow for now but should be removed later
-    inputFunctions = builtins.map (c: c.package) inputFunctions;
-
-
-    # this is needed on NixOS but does not hurt on other
-    # OSes either
-    PROTOC = "${pkgs.protobuf}/bin/protoc";
-
-    buildPhase = ''
-      export HOME=$PWD
-      cargo build --release
-    '';
-
-    checkPhase = ''
-      export HOME=$PWD
-      cargo test
-
-      cargo fmt -- --check
-
-      cargo clippy
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp target/release/avery $out/bin
-    '';
-
-    # always want backtraces when building or in dev
-    RUST_BACKTRACE = 1;
-    PROTOBUF_DEFINITIONS_LOCATION = ../../protocols;
-    shellHook = ''
-      export PROTOBUF_DEFINITIONS_LOCATION=../../protocols
-    '';
-  };
+  # TODO: This gives a nice workflow for now but should be removed later
+  inputFunctions = builtins.map (c: c.package) inputFunctions;
 }
