e94eb0a27bd377cc78f7d6fd51a5636e5b7df8e5
Sakarias Johansson <sakariasjohansson@hotmail.com>
2020-06-02 17:01:39 +0200

    ✨ Copy attachments to output folder
     * checksums is also correctly calculated
     * rename attachment files to attachment names
     * update manifest with checksums and paths

--- nedryland/manifest.nix
+++ nedryland/manifest.nix
@@ -1,55 +1,66 @@
 { pkgs, name, manifest, code, attachments }:
 let
-  genAttachmentAttrs = attachmentPathOrAttrs: name: idx:
+  genAttachmentAttrs = attachmentPathOrAttrs: attachmentName: idx:
     let
-      checksums = {sha256 = "@sha256_${builtins.toString idx}@";};
-      content = 
-    (if builtins.isString attachmentPathOrAttrs || builtins.isPath attachmentPathOrAttrs then
-        {
-          path = attachmentPathOrAttrs;
+      checksums = { checksums = { sha256 = "@sha256_${builtins.toString idx}@"; }; };
+      path = { path = "@attachment_${builtins.toString idx}@"; };
+      metadata = (
+        if builtins.isString attachmentPathOrAttrs || builtins.isPath attachmentPathOrAttrs then {
           metadata = { };
-        }
-      else
-        {
-          path = attachmentPathOrAttrs.path;
+        } else {
           metadata = attachmentPathOrAttrs.metadata or { };
         }
-    );
+      );
     in
     {
-      "${name}"= content // checksums;
+      "${attachmentName}" = ({ } // metadata // checksums // path);
     };
 
+  getAttachmentPath = attachmentPathOrAttrs:
+    (
+      if builtins.isString attachmentPathOrAttrs || builtins.isPath attachmentPathOrAttrs then
+        attachmentPathOrAttrs
+      else
+        attachmentPathOrAttrs.path
+    );
   manifestContent = if builtins.isPath manifest then (builtins.fromTOML (builtins.readFile manifest)) else manifest;
-  attachmentList = pkgs.lib.mapAttrsToList (key: value: {"${key}"= value;}) attachments;
+  attachmentList = pkgs.lib.mapAttrsToList
+    (key: value: {
+      attachmentName = "${key}";
+      data = value;
+    })
+    attachments;
+
+  nestedAttachmentList = pkgs.lib.imap1 (i: att: genAttachmentAttrs att.data att.attachmentName i) attachmentList;
+  flattenedAttachmentList = (pkgs.lib.lists.fold (a: b: a // b) { } nestedAttachmentList);
   manifestWithChecksum = manifestContent // {
-    attachments = pkgs.lib.imap1 (i: att: genAttachmentAttrs att i) attachmentList;
+    attachments = flattenedAttachmentList;
   }
-   // cgenAttachmentAttrs code "code" 0;
-
-  attachmentPaths = [ manifestWithChecksum.code.path ] ++ (builtins.map (att: att.path) manifestWithChecksum.attachments);
-
+    // genAttachmentAttrs code "code" 0;
+  # Code path is treated differently because it's relative to the installation folder rather than the component
+  # and attachment paths are always relative to the component or are an absolute path
+  # Could be possible to unify code with attachments
+  codePath = "${getAttachmentPath code}";
+  attachmentPaths = pkgs.lib.lists.forEach attachmentList (att: (getAttachmentPath att.data));
+  attachmentNames = pkgs.lib.lists.forEach attachmentList (att: (att.attachmentName));
 in
-pkgs.stdenv.mkDerivation
-{
+pkgs.stdenv.mkDerivation {
   name = "${name}-manifest";
   nativeBuildInputs = [ pkgs.utillinux pkgs.remarshal ];
 
-  inherit attachmentPaths;
+  inherit attachmentPaths codePath attachmentNames;
   manifestContent = builtins.toJSON manifestWithChecksum;
   passAsFile = [ "manifestContent" ];
 
   phases = [ "buildPhase" "installPhase" "fixupPhase" ];
 
   buildPhase = ''
-      #source $stdenv/setup
-      json2toml -i $manifestContentPath -o ./manifest.toml
-      cat ./manifest.toml
+    json2toml -i $manifestContentPath -o ./manifest.toml
   '';
 
   installPhase = ''
-      mkdir -p $out
-      cp manifest.toml $out/manifest.toml
+    mkdir -p $out
+    cp manifest.toml $out/manifest.toml
   '';
 
   setupHook = ./generate-manifest.sh;
