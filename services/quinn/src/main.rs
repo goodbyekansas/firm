f3fe22c7bd7d3fd025baea636192a48ad6947498
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2020-10-01 15:40:27 +0200

    💄 Use logger when exiting Quinn with error

--- services/quinn/src/main.rs
+++ services/quinn/src/main.rs
@@ -1,21 +1,19 @@
 #![deny(warnings)]
 
-use slog::{info, o, Drain, Logger};
+use slog::{error, info, o, Drain, Logger};
 
 use gbk_protocols::{
     functions::functions_registry_server::FunctionsRegistryServer, tonic::transport::Server,
 };
 use quinn::{config, registry};
+use std::error::Error;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
+async fn run(log: Logger) -> Result<(), Box<dyn Error>> {
+    let config_log = log.new(o!("component" => "config"));
 
-    let log = Logger::root(drain, o!());
-
-    let config = config::Configuration::new(log.new(o!("component" => "config")))?;
+    // TODO make configuration parsing async
+    let config =
+        tokio::task::spawn_blocking(move || config::Configuration::new(config_log)).await??;
 
     let addr = format!(
         "0.0.0.0:{}",
@@ -34,4 +32,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), i32> {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let log = Logger::root(drain, o!());
+
+    let exit_log = log.new(o!("component" => "exit"));
+    run(log).await.map_err(|e| {
+        error!(exit_log, "Unhandled error: {}. Exiting", e);
+        1i32
+    })
 }
