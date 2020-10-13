746fff9e73bccb88fc9d4360abae2326ed921267
Albert Cervin <albert@acervin.com>
2020-10-13 07:29:23 +0200

    ✨ Add root cert store to lomax
    This will query the system store for certificates. Reason that we do not
    bundle the app with certificates from nix is that it is bad security
    practice to not stay up to date with certificates (revocations and
    such).

--- clients/lomax/lomax.nix
+++ clients/lomax/lomax.nix
@@ -3,4 +3,6 @@ base.languages.rust.mkClient {
   name = "lomax";
   src = ./.;
   rustDependencies = [ protocols protocolsTestHelpers ];
+
+  nativeBuildInputs = pkgs.stdenv.lib.optional pkgs.stdenv.hostPlatform.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
 }
