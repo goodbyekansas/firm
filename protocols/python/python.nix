4798a37a0755157a59f77fbadbc0508bd8ed5b1c
Sakarias Johansson <sakariasjohansson@hotmail.com>
2020-10-28 09:43:37 +0100

    🔨 Rename python gbk-protocols to firm-protocols

--- protocols/python/python.nix
+++ protocols/python/python.nix
@@ -2,25 +2,25 @@
 
 base.languages.python.mkUtility {
   inherit pythonVersion;
-  name = "gbk-protocols";
+  name = "firm-protocols";
   version = "0.1.0";
   src = ../.;
   nativeBuildInputs = (pythonPkgs: [ pythonPkgs.grpcio-tools ]);
   doStandardTests = false;
   preBuild = ''
-    mkdir ./gbk_protocols
+    mkdir ./firm_protocols
     cp python/setup.py .
 
     python -m grpc_tools.protoc \
         -I . \
-        --python_out=./gbk_protocols \
-        --grpc_python_out=./gbk_protocols \
+        --python_out=./firm_protocols \
+        --grpc_python_out=./firm_protocols \
         **/*.proto
 
     # protoc does not add __init__.py files, so let's do so
-    find ./gbk_protocols -type d -exec touch {}/__init__.py \;
+    find ./firm_protocols -type d -exec touch {}/__init__.py \;
 
     # correct the imports since that is apparently impossible to do correctly
-    sed -i -E 's/^from (\S* import .*_pb2)/from gbk_protocols.\1/ ' gbk_protocols/**/*.py
+    sed -i -E 's/^from (\S* import .*_pb2)/from firm_protocols.\1/ ' firm_protocols/**/*.py
   '';
 }
