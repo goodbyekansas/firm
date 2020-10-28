9dd103689163d866c7303b3a7c688c6700542df4
Sakarias Johansson <sakariasjohansson@hotmail.com>
2020-10-28 09:43:37 +0100

    🔨 Rename python gbk-protocols to firm-protocols

--- protocols/python/setup.py
+++ protocols/python/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gbk-protocols",
+    name="firm-protocols",
     version="0.1.0",
-    url="https://github.com/goodbyekansas/protocols",
+    url="https://github.com/goodbyekansas/firm",
     author="GBK Pipeline Team",
     author_email="pipeline@goodbyekansas.com",
-    description="Python type definitions for GBK protocols",
+    description="Python type definitions for Firm protocols",
     packages=find_packages(),
 )
