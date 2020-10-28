0f668d1d08e40e2b6a86bbdc8dd69b54292a7bb3
Albert Cervin <albert@acervin.com>
2020-10-28 09:36:00 +0100

    ⬆️ Update Nedryland to get rust warnings as errors
    Remove all temp settings that did the same thing

--- clients/bendini/bendini.nix
+++ clients/bendini/bendini.nix
@@ -2,7 +2,6 @@
 base.languages.rust.mkClient {
   name = "bendini";
   src = ./.;
-  RUSTFLAGS = "-D warnings"; # TODO: This should be remove once nedryland has been updated with default
   rustDependencies = [ protocols protocolsTestHelpers tonicMiddleware ];
   nativeBuildInputs = [ pkgs.pkg-config pkgs.openssl ]
     ++ pkgs.stdenv.lib.optional pkgs.stdenv.hostPlatform.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
