8bfc00dd8791e8bd8001506ef83ea2a0bca7ca14
Albert Cervin <albert@acervin.com>
2020-11-08 15:14:51 +0100

    ⬆️ Update nedryland to get vendoring
    Also refactor proto slightly and make all needed changes to support
    vendoring

--- protocols/python/python.nix
+++ protocols/python/python.nix
@@ -1,23 +1,22 @@
-{ base, pkgs, pythonVersion ? pkgs.python3 }:
+{ base, pkgs, protoSources, pythonVersion ? pkgs.python3 }:
 
 base.languages.python.mkUtility {
-  inherit pythonVersion;
+  inherit pythonVersion protoSources;
   name = "firm-protocols";
   version = "0.1.0";
-  src = ../.;
   nativeBuildInputs = (pythonPkgs: [ pythonPkgs.grpcio-tools pythonPkgs.mypy-protobuf pythonPkgs.mypy ]);
+  src = ./.;
   propagatedBuildInputs = (pythonPkgs: [ pythonPkgs.grpcio ]);
   doStandardTests = false;
   preBuild = ''
     mkdir ./firm_protocols
-    cp python/setup.py .
 
     python -m grpc_tools.protoc \
-        -I . \
+        -I $protoSources \
         --python_out=./firm_protocols \
         --grpc_python_out=./firm_protocols \
         --mypy_out=./firm_protocols \
-        **/*.proto
+        $protoSources/**/*.proto
 
     # protoc does not add __init__.py files, so let's do so
     find ./firm_protocols -type d -exec touch {}/__init__.py \;
