71a60d27c671888da00c0254c1bf428676f4da20
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2020-11-17 10:13:56 +0100

    ⚡ Make tar.gz of attachment if it is a folder

--- extensions/generate-manifest.sh
+++ extensions/generate-manifest.sh
@@ -4,23 +4,23 @@ generateManifestPhase() {
 
   echo "  📜 [manifest] Creating output manifest..."
 
-  echo "  📜 [manifest] Generating checksums for attachments..."
-  python @out@/generate_checksums.py \
-          @out@/manifest-data.json \
-          ./manifest-data-with-checksums.json \
-          --sha512 | sed "s/^/  📜 [manifest] /"
-
   echo "  📜 [manifest] Copying attachments to output folder..."
   mkdir -p $out/attachments
   python @out@/copy_attachments.py \
-          $out/attachments \
-          ./manifest-data-with-checksums.json \
-          ./manifest-data-with-existing-attachments.json | sed "s/^/  📜 [manifest] /"
+    $out/attachments \
+    @out@/manifest-data.json \
+    ./manifest-data-with-existing-attachments.json | sed "s/^/  📜 [manifest] /"
+
+  echo "  📜 [manifest] Generating checksums for attachments..."
+  python @out@/generate_checksums.py \
+    ./manifest-data-with-existing-attachments.json \
+    ./manifest-data-with-checksums.json \
+    --sha512 | sed "s/^/  📜 [manifest] /"
 
   echo "  📜 [manifest] Generating final manifest..."
   j2 -f json --customize @out@/template_settings.py \
-          @out@/manifest-template.jinja.toml \
-          ./manifest-data-with-existing-attachments.json \
-          -o $out/manifest.toml | sed "s/^/  📜 [manifest] /"
+    @out@/manifest-template.jinja.toml \
+    ./manifest-data-with-checksums.json \
+    -o $out/manifest.toml | sed "s/^/  📜 [manifest] /"
   echo "  📜 [manifest] Manifest written to $out/manifest.toml"
 }
