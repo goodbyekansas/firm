56c813bdab1a88c300fbcdf82f66544c7a005a71
Simon Rainerson <simon.rainerson@goodbyekansas.com>
2020-11-18 11:16:30 +0100

    ⬆️ 🔥 Move dcc_integrations out of Firm

--- protocols/protocols.nix
+++ protocols/protocols.nix
@@ -1,8 +1,20 @@
 { base, pkgs }:
-base.languages.protobuf.mkModule {
-  name = "firm-protocols";
-  src = ./.;
-  version = "0.1.0";
-  languages = [ "rust" "python" ];
-  pythonVersion = pkgs.python3;
+rec{
+  source = ./.;
+  withoutServices = base.languages.protobuf.mkModule {
+    name = "firm-protocols";
+    src = source;
+    version = "0.1.0";
+    languages = [ "rust" "python" ];
+    pythonVersion = pkgs.python3;
+    includeServices = false;
+  };
+  withServices = base.languages.protobuf.mkModule {
+    name = "firm-protocols";
+    src = source;
+    version = "0.1.0";
+    languages = [ "rust" "python" ];
+    pythonVersion = pkgs.python3;
+    includeServices = true;
+  };
 }
