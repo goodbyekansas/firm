d8723527f7a8f0196aa20fb258fa1a13256d8d54
Albert Cervin <albert@acervin.com>
2020-11-26 08:48:36 +0100

    ⚡️ Add config file to ignores + macos build fixes

--- services/avery/avery.nix
+++ services/avery/avery.nix
@@ -5,4 +5,6 @@ base.languages.rust.mkService {
   src = ./.;
   externalDependenciesHash = "sha256-sh8Ea6t1BhxHu1R0XkSbYAWVT0YkJbnR8q/8Riz253k=";
   buildInputs = [ types.package ] ++ pkgs.stdenv.lib.optional pkgs.stdenv.hostPlatform.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
+
+  nativeBuildInputs = pkgs.stdenv.lib.optional pkgs.stdenv.hostPlatform.isDarwin pkgs.xcbuild;
 }
