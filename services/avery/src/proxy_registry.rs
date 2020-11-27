21fed293485d0a592ccb72b8b8bd260cba2cd6c5
Albert Cervin <albert@acervin.com>
2020-11-27 13:53:48 +0100

    ✨ Add registry metadata on list and get
    The metadata key `registry` will contain the name of the registry that
    yielded the function.

--- services/avery/src/proxy_registry.rs
+++ services/avery/src/proxy_registry.rs
@@ -1,20 +1,28 @@
-use futures::{stream, StreamExt, TryStreamExt};
-use thiserror::Error;
-use url::Url;
+use std::collections::{hash_map::Entry, HashMap};
 
-use crate::{config::ConflictResolutionMethod, registry::RegistryService};
 use firm_types::{
     functions::{registry_client::RegistryClient, registry_server::Registry},
     tonic,
 };
+use futures::{stream, StreamExt, TryStreamExt};
 use slog::{info, o, Logger};
-use std::collections::{hash_map::Entry, HashMap};
+use thiserror::Error;
 use tonic::transport::{ClientTlsConfig, Endpoint};
 use tonic_middleware::HttpStatusInterceptor;
+use url::Url;
+
+use crate::{config::ConflictResolutionMethod, registry::RegistryService};
+
+#[derive(Debug, Clone)]
+struct RegistryConnection {
+    name: String,
+    client: RegistryClient<HttpStatusInterceptor>,
+}
+
 #[derive(Debug, Clone)]
 pub struct ProxyRegistry {
     internal_registry: RegistryService,
-    channels: Vec<RegistryClient<HttpStatusInterceptor>>,
+    connections: Vec<RegistryConnection>,
     conflict_resolution: ConflictResolutionMethod,
     log: Logger,
 }
@@ -107,15 +115,20 @@ impl ProxyRegistry {
     ) -> Result<Self, ProxyRegistryError> {
         Ok(Self {
             internal_registry,
-            channels: stream::iter(external_registries)
-                .then(|er| {
+            connections: stream::iter(external_registries)
+                .then(|er| async {
                     let reg_name = er.name.clone();
-                    create_connection(
-                        er,
-                        log.new(o!("scope" => "connect", "registry" => reg_name)),
-                    )
+
+                    Ok::<RegistryConnection, ProxyRegistryError>(RegistryConnection {
+                        name: reg_name.clone(),
+                        client: create_connection(
+                            er,
+                            log.new(o!("scope" => "connect", "registry" => reg_name)),
+                        )
+                        .await?,
+                    })
                 })
-                .try_collect::<Vec<RegistryClient<HttpStatusInterceptor>>>()
+                .try_collect::<Vec<RegistryConnection>>()
                 .await?,
             conflict_resolution,
             log,
@@ -134,41 +147,61 @@ impl Registry for ProxyRegistry {
     > {
         let payload = request.into_inner();
         Ok(tonic::Response::new(firm_types::functions::Functions {
-            functions:
-                stream::iter(
-                    self.channels
-                        .iter()
-                        .map(|client| (client.clone(), payload.clone())),
-                )
-                .then(|(mut client, payload)| async move {
-                    client.list(tonic::Request::new(payload)).await
-                })
-                .chain(stream::once(
+            functions: stream::iter(
+                self.connections
+                    .iter()
+                    .map(|connection| (connection.clone(), payload.clone())),
+            )
+            .then(|(mut connection, payload)| async move {
+                connection
+                    .client
+                    .list(tonic::Request::new(payload))
+                    .await
+                    .map(|functions| (connection.name.clone(), functions))
+            })
+            .chain(
+                stream::once(
                     self.internal_registry
                         .list(tonic::Request::new(payload.clone())),
-                ))
-                .try_collect::<Vec<tonic::Response<firm_types::functions::Functions>>>()
-                .await?
-                .into_iter()
-                .flat_map(|response| response.into_inner().functions)
-                .try_fold(HashMap::new(), |mut hashmap, function| {
-                    match hashmap.entry(format!("{}:{}", function.name, function.version)) {
-                        Entry::Occupied(existing) => match self.conflict_resolution {
-                            ConflictResolutionMethod::Error => Err(
-                                ProxyRegistryError::ConflictingFunctions(existing.key().clone()),
-                            ),
-                            ConflictResolutionMethod::UsePriority => Ok(hashmap),
-                        },
-                        Entry::Vacant(vacant) => {
-                            vacant.insert(function);
-                            Ok(hashmap)
-                        }
+                )
+                .map(|f| f.map(|functions| (String::from("internal"), functions))),
+            )
+            .try_collect::<Vec<(String, tonic::Response<firm_types::functions::Functions>)>>()
+            .await?
+            .into_iter()
+            .map(|(name, mut functions)| {
+                // insert metadata
+                functions
+                    .get_mut()
+                    .functions
+                    .iter_mut()
+                    .for_each(|function| {
+                        function
+                            .metadata
+                            .insert("registry".to_owned(), name.clone());
+                    });
+
+                functions
+            })
+            .flat_map(|functions| functions.into_inner().functions)
+            .try_fold(HashMap::new(), |mut hashmap, function| {
+                match hashmap.entry(format!("{}:{}", function.name, function.version)) {
+                    Entry::Occupied(existing) => match self.conflict_resolution {
+                        ConflictResolutionMethod::Error => Err(
+                            ProxyRegistryError::ConflictingFunctions(existing.key().clone()),
+                        ),
+                        ConflictResolutionMethod::UsePriority => Ok(hashmap),
+                    },
+                    Entry::Vacant(vacant) => {
+                        vacant.insert(function);
+                        Ok(hashmap)
                     }
-                })
-                .map_err(|e| tonic::Status::already_exists(e.to_string()))?
-                .into_iter()
-                .map(|(_, v)| v)
-                .collect::<Vec<firm_types::functions::Function>>(),
+                }
+            })
+            .map_err(|e| tonic::Status::already_exists(e.to_string()))?
+            .into_iter()
+            .map(|(_, v)| v)
+            .collect::<Vec<firm_types::functions::Function>>(),
         }))
     }
 
@@ -182,22 +215,35 @@ impl Registry for ProxyRegistry {
         let payload = request.into_inner();
 
         let res = stream::iter(
-            self.channels
+            self.connections
                 .iter()
                 .map(|client| (client.clone(), payload.clone())),
         )
-        .then(|(mut client, payload)| async move { client.get(tonic::Request::new(payload)).await })
-        .chain(stream::once(
-            self.internal_registry
-                .get(tonic::Request::new(payload.clone())),
-        ))
-        .collect::<Vec<Result<tonic::Response<firm_types::functions::Function>, tonic::Status>>>()
+        .then(|(mut connection, payload)| async move {
+            connection
+                .client
+                .get(tonic::Request::new(payload))
+                .await
+                .map(|functions| (connection.name.clone(), functions))
+        })
+        .chain(
+            stream::once(
+                self.internal_registry
+                    .get(tonic::Request::new(payload.clone())),
+            )
+            .map(|f| f.map(|functions| (String::from("internal"), functions))),
+        )
+        .collect::<Vec<Result<(String, tonic::Response<firm_types::functions::Function>), tonic::Status>>>()
         .await
         .into_iter()
         .filter(|v| !matches!(v, Err(e) if e.code() == tonic::Code::NotFound))
-        .collect::<Result<Vec<tonic::Response<firm_types::functions::Function>>, tonic::Status>>()?
+        .collect::<Result<Vec<(String, tonic::Response<firm_types::functions::Function>)>, tonic::Status>>()?
         .into_iter()
-        .map(|response| response.into_inner())
+        .map(|(registry_name, response)| {
+            let mut r = response.into_inner();
+            r.metadata.insert("registry".to_owned(), registry_name);
+            r
+        })
         .try_fold(HashMap::new(), |mut hashmap, function| {
             match hashmap.entry(format!("{}:{}", function.name, function.version)) {
                 Entry::Occupied(existing) => match self.conflict_resolution {
