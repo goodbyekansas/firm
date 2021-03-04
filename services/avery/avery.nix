82aea6cc44b7c3ab52f5628726ced36fdaa87bd6
Simon Rainerson <simonr88@gmail.com>
2021-03-04 17:48:55 +0100

    🔨 ⬆️ Rename utils to libraries
    Also move runtimes into avery since they are both created inside firm and
    therefor are not extensions. Added srcExclude to not have runtimes be
    considered the source to avery.

--- services/avery/avery.nix
+++ services/avery/avery.nix
@@ -3,6 +3,10 @@ with pkgs;
 base.languages.rust.mkService {
   name = "avery";
   src = ./.;
+  srcExclude = [
+    (path: type: (type == "directory" && baseNameOf path == "runtimes"))
+    (path: type: (type == "regular" && baseNameOf path == "avery-with-runtimes.nix"))
+  ];
   buildInputs = [ types.package tonicMiddleware.package ]
     ++ pkgs.stdenv.lib.optional pkgs.stdenv.hostPlatform.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
   nativeBuildInputs = pkgs.stdenv.lib.optional pkgs.stdenv.hostPlatform.isDarwin pkgs.xcbuild;
