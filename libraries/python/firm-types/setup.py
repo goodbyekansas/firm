64d5b2ea69852f0f27bb01487485ceb1003c4f17
Sakarias Johansson <sakarias.johansson@goodbyekansas.com>
2021-07-02 17:29:10 +0200

    💎 Firm Version 1.0.0

--- libraries/python/firm-types/setup.py
+++ libraries/python/firm-types/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="firm-types",
-    version="0.1.0",
+    version="1.0.0",
     url="https://github.com/goodbyekansas/firm",
     author="GBK Pipeline Team",
     author_email="pipeline@goodbyekansas.com",
--- services/avery/runtimes/python/examples/networking/networking.nix
+++ services/avery/runtimes/python/examples/networking/networking.nix
@@ -1,7 +1,7 @@
 { base, pkgs }:
 base.languages.python.mkFunction {
   name = "networking";
-  version = "0.1.0";
+  version = "1.0.0";
   src = ./.;
   entrypoint = "networking:main";
 
