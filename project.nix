b636bd279de755e54588abbf6b58a90b745eae04
Simon Rainerson <simonr88@gmail.com>
2021-10-21 09:17:23 +0200

    💎 Firm version 1.2.1

--- project.nix
+++ project.nix
@@ -5,7 +5,7 @@ let
 in
 nedryland.mkProject rec {
   name = "firm";
-  version = "1.2.0";
+  version = "1.2.1";
   baseExtensions = [
     ./extensions/nedryland/function.nix
     ./extensions/nedryland/runtime.nix
