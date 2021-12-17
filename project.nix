83bddf1a4793dcdfe694b77c83ac6de9a119fad3
Albert Cervin <albert.cervin@goodbyekansas.com>
2021-12-17 17:13:45 +0100

    🐛 Set correct version for Firm

--- project.nix
+++ project.nix
@@ -5,7 +5,7 @@ let
 in
 nedryland.mkProject rec {
   name = "firm";
-  version = "2.0.1";
+  version = "2.0.2";
   baseExtensions = [
     ./extensions/nedryland/function.nix
     ./extensions/nedryland/runtime.nix
